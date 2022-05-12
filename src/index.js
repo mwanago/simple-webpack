e5845f1b15585668b02834625fef12ff544cf863
marcin <wanago.marcin@gmail.com>
2022-05-12 19:00:01 +0200

    chore(): add the basics of the webpack configuration

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.css';
+
+const paragraph = document.createElement('p');
+paragraph.innerText = 'Hello!';
+document.body.append(paragraph);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[chunkhash].bundle.js',
+  },
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
