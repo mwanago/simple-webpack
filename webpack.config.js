5e329a090f1e07201ebe3af243641c0bfe84f90e
Marcin <wanago.marcin@gmail.com>
2023-06-28 20:41:33 +0200

    feat(Webpack): used eval-source-map for better source maps

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].[chunkhash].bundle.js',
   },
+  devtool: 'eval-source-map',
   mode: 'development',
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   module: {
